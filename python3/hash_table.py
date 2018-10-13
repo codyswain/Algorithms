933ac3d60152d6f5c319d68780c1081caa2c60db
Cody Swain <swain.cody@gmail.com>
2018-10-13 13:17:30 -0700

    Moved hash table; added get(); __getitem__; __setitem__

--- python3/hash_table.py
+++ python3/hash_table.py
@@ -1,7 +1,6 @@
 __author__ = "Cody Swain"
 
 '''Creating custom dict'''
-
 class HashTable:
 	def __init__(self):
 		self.size = 10
@@ -36,3 +35,27 @@ class HashTable:
 					self.data[nextslot] = data
 				else:
 					self.data[nextslots] = data #replace existing data
+
+	def get(self, key):
+		startslot = self.hashfunction(key, len(self.slots))
+		data = None
+		stop = False
+		found = False
+		position = startslot
+		while self.slots[position] != None and not found and not stop:
+			if self.slots[position] == key:
+				found = True
+				data = self.data[position]
+			else:
+				position = self.rehash(position, len(self.slots))
+				if position == startslot:
+					stop = True
+		return data
+
+	def __getitem__(self, key):
+		return self.get(key)
+
+	def __setitem__(self, key, data):
+		self.put(key, data)
+
+
