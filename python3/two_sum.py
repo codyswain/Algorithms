e118e9a62afebf6bff0e8e41e46685526ce764a1
Cody Swain <swain.cody@gmail.com>
2019-04-12 23:12:04 -0700

    Revised solution and added description

--- python3/two_sum.py
+++ python3/two_sum.py
@@ -1,17 +1,27 @@
 __author__ = "Cody Swain"
 
-class Solution(object):
-	def get_two_sum(self, array, two_sum):
-		previous_nums = dict()
-		for index, num in enumerate(array):
-			complement = two_sum - num
-			if complement in previous_nums:
-				return previous_nums[complement], index
-			else: 
-				previous_nums[num] = index
+''' Strategy: 
+    -------------------------------------------------------------------------------------------
+	Loop over the values in the list. 
+	For each element, calculate the necessary complement required to obtain the target
+	Store the index of the element with the key as the complement
+	When the complement is reached, return the value stored at key, and the idx of complement
 
-		return -1
+	Complexity Analysis: 
+	The runtime complexity is O(n) because in the worst case scenario, you do a linear scan
+	of the entire list. Given a large ordered list, it may be beneficial to run a binary search 
+	for the complement value. 
+'''
+
+class Solution(object):
+	def twoSum(self, nums, target): 
+		complements = {}
+		for idx, val in enumerate(nums):
+			if val in complements.keys():
+				return complements[val], idx
+			else:
+				complements[target-val] = idx
 
 if __name__ == "__main__":
 	test_array = [1, 4, 5, 3, 6, 8, 9, 2, 10, 13]
-	print("Indices of two numbers which give sum: {}".format(Solution().get_two_sum(test_array, 14)))
+	print("Indices of two numbers which give sum: {}".format(Solution().twoSum(test_array, 14)))
