a96e66f0ddaa1d8bfdf5b8a5f9a6e3b948d7c036
Cody Swain <swain.cody@gmail.com>
2019-08-21 13:38:03 -0700

    Leetcode medium: Expand from center solution is correct

--- python3/longest-palindromic-substring.py
+++ python3/longest-palindromic-substring.py
@@ -4,35 +4,8 @@ Leetcode Problem: #5 Longest Palindromic Substring'''
 import numpy as np
 
 class Solution():
-	def longest_palindrome(self, s: str) -> str:
-		length = len(s)
-		if s == "" or length < 2:
-			return s
-
-		left = 0
-		right = 0
-
-		isPalindrome = [[False]*length]*length
-		
-		for j in range(length):
-			for i in range(j):
-				if j-1 <= 2:
-					isInnerPalindrome = True
-				elif isPalindrome[i+1][j-1]:
-					isInnerPalindrome = True
-				else:
-					isInnerPalindrome = False
-
-				if (s[i] == s[j] and isInnerPalindrome):
-					isPalindrome[i][j] = True;
-
-					if j-i > right-left:
-						left = i
-						right = j
-
-		return s[left: right+1]
-
-	def longest_palindromic_substring(self, s: str) -> str:
+	def dynamic_programming_solution(self, s: str) -> str:
+		''' This solution is not complete'''
 		length = len(s)
 		palindrome = np.zeros((length, length+1), dtype=bool)
 
@@ -58,10 +31,63 @@ class Solution():
 				if s[i] == s[i+k-1] and palindrome[i+1][k-2]:
 					print("Palindrome from: {}".format(s[i:i+k]))
 		print(palindrome)
+
+	def expand_from_center_solution(self, s: str) -> str:
+		''' 
+		This solution works.
+
+		Time complexity: O(N^2). For each element in the for loop you
+		have a worst case expansion of the whole strin O(N*N)
+
+		Space Complexity: Ideally it should be O(1). You don't save an NxN 
+		matrix like in the dynamic programming solution. In this case however,
+		it is probably  worst case O(N^2) because I have to allocated space
+		for a new string copy every time. 
+
+		Improvements: 
+		------------------------------------------------------------------------
+		The while loops could be placed
+		into a separate helper function to remove code redundancy.
+
+		You also could save the indices instead of constantly creating
+		copies of the string via temp (strings are immutable in python)
+		which means a copy of the object is created. 
+		''' 
+		length = len(s) 
+		longest = ""
+		temp = ""
+		for i in range(length):
+			l = i
+			r = i
+
+			# Expand from individual chars
+			while l>=0 and r<=length-1 and s[l]==s[r]:
+				if s[l] == s[r]:
+					temp = s[l:r+1]
+					print("Temp 1st: {}".format(temp))
+				if len(temp) > len(longest):
+					longest = temp
+				l -= 1
+				r += 1
+
+			# Expand from in between 2 char palindrome
+			if i+1 <= length-1:
+				if s[i] == s[i+1]:
+					l = i
+					r = i+1
+					while l>=0 and r<=length-1 and s[l]==s[r]:
+						temp = s[l:r+1]
+						if len(temp) > len(longest):
+							longest = temp
+						l -= 1
+						r += 1
+					
+		return longest
 					
 		
 if __name__ == "__main__":
 	test_str = "babaadaa"
 	sol = Solution()
-	sol.longest_palindromic_substring(test_str)
+	longest_palindrome = sol.expand_from_center_solution(test_str)
+	print(longest_palindrome)
 		
